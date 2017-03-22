f1ac1fd9ad0df7f56a80553b8e194f752e126d8b
ktracaengemark-AOC <marciorodeng@gmail.com>
2017-03-22 07:46:44 -0300

    Acerto do menu principal
    Apenas uma melhoria

--- application/views/basico/nav_principal.php
+++ application/views/basico/nav_principal.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
 
-    <div class="row">
+     <div class="row">
         <div class="col-md-9">
 
             <ul class="nav navbar-nav navbar-left"> 			
@@ -58,9 +58,9 @@
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Financeiro<span class="caret"></span></a>
 					<ul class="dropdown-menu">
-						<li><a href="<?php echo base_url() ?>cliente/pesquisar">Orçamentos / Entradas</a></li>
+						<li><a href="<?php echo base_url() ?>cliente/pesquisar">Orçamentos & Entradas</a></li>
 						<li role="separator" class="divider"></li>
-						<li><a href="<?php echo base_url() ?>despesa/cadastrar/despesa">Despesas / Saídas</a></li>
+						<li><a href="<?php echo base_url() ?>despesa/cadastrar/despesa">Despesas & Saídas</a></li>
 						<li role="separator" class="divider"></li>
 
 					</ul>
@@ -75,11 +75,11 @@
 						<li role="separator" class="divider"></li>
 						<li><a href="<?php echo base_url() ?>relatorio/empresas">Empresas & Fornec.</a></li>
 						<li role="separator" class="divider"></li>
-						<li><a href="<?php echo base_url() ?>relatorio/orcamentopc">Orçamentos / Procedimentos</a></li>
+						<li><a href="<?php echo base_url() ?>relatorio/orcamentopc">Orçamentos & Procedimentos</a></li>
 						<li role="separator" class="divider"></li>
-						<li><a href="<?php echo base_url() ?>relatorio/orcamento">Orçamentos / Entradas</a></li>
+						<li><a href="<?php echo base_url() ?>relatorio/orcamento">Orçamentos & Pagamentos</a></li>
 						<li role="separator" class="divider"></li>
-						<li><a href="<?php echo base_url() ?>relatorio/despesa">Despesas / Saídas</a></li>
+						<li><a href="<?php echo base_url() ?>relatorio/despesa">Despesas & Saídas</a></li>
 						<li role="separator" class="divider"></li>
 						<li><a href="<?php echo base_url() ?>relatorio/financeiro">Balanço</a></li>
 						<li role="separator" class="divider"></li>					
