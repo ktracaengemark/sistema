6e4c3fbe3a48dd2db04a06c67f60f7ea52bbae0e
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-07-05 10:01:42 -0300

    esqueci de mudar a database....kkkkk

--- application/config/database.php
+++ application/config/database.php
@@ -102,13 +102,13 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 
-    #'hostname' => '159.203.125.243',
-	#'username' => 'usuario',
-	#'password' => '20UtpJ15',
+    'hostname' => '159.203.125.243',
+	'username' => 'usuario',
+	'password' => '20UtpJ15',
 
-    'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',    
+    #'hostname' => 'localhost',
+	#'username' => 'root',
+	#'password' => '',    
 
     #'database' => 'ci',
     'database' => 'app',
