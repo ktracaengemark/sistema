4b234241757c816be3b3fc8fff56e38416d1ab32
marciorodeng <marciorodeng@gmail.com>
2021-08-03 10:31:30 -0300

    Ajuste no texto do cadastro das Empresas

--- application/views/loginempresa/form_registrar.php
+++ application/views/loginempresa/form_registrar.php
@@ -200,11 +200,11 @@
 											<div class="modal-header bg-danger">
 												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 												<h4 class="modal-title">Ao "Confirmar Registro",<br>
-																		Você concorda com a nossa Política de Privacidade!</h4>
+																		você terá 15 dias, GRÁTIS, para testar o nosso sistema!</h4>
 											</div>
 											
 											<div class="modal-body">
-												<p>Política de Privacidade.....</p>
+												<p>15 dias Grátis!!</p>
 											</div>
 											
 											<div class="modal-footer">
