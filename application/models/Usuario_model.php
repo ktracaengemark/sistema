0fc81848313376950978c9b48d4e9c454b5d7728
marciorodeng <marciorodeng@gmail.com>
2022-02-01 15:19:19 -0300

    Auto preenchimento do agenda

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -78,7 +78,17 @@ class Usuario_model extends CI_Model {
     }
 	
     public function get_usuario($data) {
-        $query = $this->db->query('SELECT * FROM Sis_Usuario WHERE idSis_Usuario = ' . $data);
+        $query = $this->db->query('
+			SELECT 
+				U.*,
+				A.idApp_Agenda
+			FROM 
+				Sis_Usuario AS U
+				 LEFT JOIN Sis_Associado AS ASS ON ASS.idSis_Associado = U.idSis_Associado
+				 LEFT JOIN App_Agenda AS A ON A.idSis_Associado = ASS.idSis_Associado
+			WHERE 
+				U.idSis_Usuario = ' . $data . '
+		');
 
         $query = $query->result_array();
 
